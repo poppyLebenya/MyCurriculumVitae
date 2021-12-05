674bf1117802ce2e582446d29d60e6ae48540e5f
poppyLebenya <moiponelebenya@gmail.com>
2021-12-05 22:35:32 +0200

    .

---
+++ Main.js
@@ -0,0 +1,40 @@
+const showMenu = (nav_toogle, nav_menu) =>{
+    const toggle = document.getElementById(nav_toogle)
+    const nav = document.getElementById(nav_menu)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show_menu')
+        })
+    }
+}
+showMenu('nav_toggle','nav_menu')
+
+//------------------------------------------------------------
+const navLink = document.querySelectorAll('nav__link')
+
+function linkAction(){
+    const nav_menu = document.getElementById('nav_menu')
+    nav_menu.classList.remove('show_menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//-------------------------------------------------------------
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
