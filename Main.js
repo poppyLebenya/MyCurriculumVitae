98464684741b0bbd20a3ae59227fcf4fd655daab
poppyLebenya <moiponelebenya@gmail.com>
2021-12-06 20:04:28 +0200

    .

--- Main.js
+++ Main.js
@@ -41,3 +41,36 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 //-----------------------------------------------------------------
+function scrollToTop(){
+    const scrollTop = document.getElementById('scrolltotop');
+    if(this.scrollX >=150) scrollTop.classList.add('show_scroll'); else scrollTop.classList.remove('show_scroll')
+}
+window.addEventListener('scroll', scrollTop)
+//-----------------------------------------------------------------
+const themeButton = document.getElementById('theme_button')
+const darkTheme = 'dark_theme'
+const iconTheme = 'light_sun'
+
+
+const selectedTheme = localStorage.getItem('selected_theme')
+const selectedIcon = localStorage.getItem('selected_icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', () => {
+    
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+  
+    localStorage.setItem('selected_theme', getCurrentTheme())
+    localStorage.setItem('selected_icon', getCurrentIcon())
+})
+
